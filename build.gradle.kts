0e2e3873cebd0aa3d8e89f56b2bbe2874efb657c
Thomas Pucci <thomas.lb.pucci@gmail.com>
2020-11-07 18:34:40 +0100

    feat: init project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("multiplatform") version "1.3.72"
+}
+
+group = "xyz.pucci"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
