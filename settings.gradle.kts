857c339f487b2561fc366a39c48ba0cdb287684c
Thomas Pucci <thomas.lb.pucci@gmail.com>
2021-07-09 11:13:58 +0200

    chore: prepare project for multi-module

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
-        mavenCentral()
-        maven("https://plugins.gradle.org/m2/")
-    }
-}
-rootProject.name = "kstate"
-
