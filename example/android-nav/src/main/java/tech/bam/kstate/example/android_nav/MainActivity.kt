35e6255c8061b25fda3411854f4573f14f8d433d
Thomas Pucci <thomas.lb.pucci@gmail.com>
2021-07-13 11:53:59 +0200

    chore: create example

---
+++ example/android-nav/src/main/java/tech/bam/kstate/example/android_nav/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.bam.kstate.example.android_nav
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
